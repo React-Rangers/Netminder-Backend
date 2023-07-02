7200e95f1c6248dd25c2da7e147175dfcc9c57ae
Kar Sodhi <karsodhi1212@gmail.com>
2023-07-02 11:29:38 -0500

    updated profile.js

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -3,40 +3,40 @@ const bcrypt = require('bcrypt');
 
 
 const profileSchema = new Schema({
-  usernamee: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  tasks: [
-    {
-        dateCreated: {
-            type: Date,
-            default: Date.now,
-        },
-        reminderDate: {
-            type: Date,
-            default: Date.now,
-        },
-        contactFirstName: {
-            type: String,
-            required: true,
-},
-          contactLastName: {
-              type: String,
-              required: true,
-          },
+    usernamee: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+    },
+    tasks: [
+        {
+            dateCreated: {
+                type: Date,
+                default: Date.now,
+            },
+            reminderDate: {
+                type: Date,
+                default: Date.now,
+            },
+            contactFirstName: {
+                type: String,
+                required: true,
+            },
+            contactLastName: {
+                type: String,
+                required: true,
+            },
             contactEmail: {
                 type: String,
                 required: false,
@@ -46,25 +46,26 @@ const profileSchema = new Schema({
                 required: false,
             },
 
-         taskDescription: { 
-            type: String,
-            required: true,
-        },  
-}]);
+            taskDescription: {
+                type: String,
+                required: true,
+            },
+        }]
+});
 
 // set up pre-save middleware to create password
 profileSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
 
-  next();
+    next();
 });
 
 // compare the incoming password with the hashed password
 profileSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password);
 };
 
 const Profile = model('Profile', profileSchema);
