be7f406ea8002cf27e099baecc6324d1ebacf656
Ryan <pinkstonr54@gmail.com>
2023-07-03 20:35:32 -0500

    minor changes to try to get apollo server to work

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -20,9 +20,8 @@ const typeDefs = gql`
     }
 
     type Query {
-        profiles: [Profile]!
+        profiles: Profile
         profile(profileId: ID!): Profile
-        # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
         me: Profile
     }
 `
